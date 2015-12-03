d7610887e036f098dc2fd2121497a1590d41c452
wgn2015 <laogao008@gmail.com>
2015-12-03 19:49:36 +0800

    公司电脑连接测试20151203

--- test1/xn.java
+++ test1/xn.java
@@ -1,9 +1,11 @@
- class TestN{
+﻿ class TestN{
 	public static void main(String [] args ){
 	int x = 1;
 	int y = 2;
 	System.out.println("hello xn"+(x+y));
 	
 	//测试修改1
+//compay computer测试
 	}
 }
+
